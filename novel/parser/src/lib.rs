e2fe25f3d09ca81ca107e58be7c89f6b1f37577c
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2024-05-18 01:34:40 +0000

    added tokenizer and parser packages

---
+++ novel/parser/src/lib.rs
@@ -0,0 +1,21 @@
+use tokenizer;
+
+pub fn parse(text: &str) -> String {
+    let mut result: String = String::from(text);
+    result
+}
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- novel/src/main.rs
+++ novel/src/main.rs
@@ -1,3 +1,10 @@
+use std::fs::read_to_string;
+use parser;
+
 fn main() {
-    println!("Hello, world!");
+    let text = match read_to_string("./src/code/First.nvl")  {
+        Ok(val) => val,
+        Err(_e) => format!("Error: {}",_e),
+    };
+    println!("{}",parser::parse(&text));
 }
