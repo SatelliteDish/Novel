dfc3cdd834c73c35adf6d1595b1846c5c8f1d3bf
SatelliteDish <94733164+SatelliteDish@users.noreply.github.com>
2024-05-19 05:16:43 +0000

    changed JSON tree to binary tree. Tokens are now structs

--- novel/parser/src/lib.rs
+++ novel/parser/src/lib.rs
@@ -1,13 +1,69 @@
 use tokenizer::{self, Tokenizer};
+use std::rc::*;
+
+enum TreeNode {
+    Addition(char,Weak<TreeNode>,Weak<TreeNode>),
+    Subtraction(char,Weak<TreeNode>,Weak<TreeNode>),
+    Multiplication(char,Weak<TreeNode>,Weak<TreeNode>),
+    Division(char,Weak<TreeNode>,Weak<TreeNode>),
+    Negation(char,Weak<TreeNode>),
+    NumericLiteral(f64)
+}
+
+impl TreeNode {
+    pub fn to_string(&self) -> String {
+        match self {
+            TreeNode::Addition(symbol,left,right) |
+            TreeNode::Subtraction(symbol,left,right) |
+            TreeNode::Multiplication(symbol,left,right) |
+            TreeNode::Division(symbol,left,right) => {
+                let left_text = match left.upgrade() {
+                    Some(node) => node.to_string(),
+                    None => "null".to_string()
+                };
+                let right_text = match right.upgrade() {
+                    Some(node) => node.to_string(),
+                    None => "null".to_string()
+                };
+                format!("
+                    '{}': {{
+                        'left': {{
+                            {}
+                        }},
+                        'right': {{
+                            {}
+                        }}
+                    }}
+                ",symbol,left_text,right_text)
+            },
+            TreeNode::Negation(symbol,arg) => {
+                let arg_text = match arg.upgrade() {
+                    Some(node) => node.to_string(),
+                    None => {"null".to_string()}
+                };
+                format!("
+                    {} : {{
+                        'arg': {{
+                            {}
+                        }} 
+                    }}
+                ",symbol,arg_text)
+            },
+            TreeNode::NumericLiteral(val) => {
+                val.to_string()
+            }
+        }
+    }
+}
 
 pub fn parse(text: &str) -> String {
     let mut tokenizer = Tokenizer::new(text);
     let mut more_tokens = true;
     let mut result = String::new();
     while more_tokens {
-        match tokenizer.next_token() {
+        match tokenizer.get_next_token() {
             Some(token) => {
-                result = result + "\n" + &token;
+                result = result + "\n" + &token.to_string();
             },
             None => {
                 more_tokens = false;
--- novel/tokenizer/src/lib.rs
+++ novel/tokenizer/src/lib.rs
@@ -1,37 +1,65 @@
-use json::{self, object};
 use regex::Regex;
-#[derive(PartialEq, Eq, Hash, Clone)]
 
+pub enum TokenType {
+    NumericLiteral,
+    StringLiteral,
+    Whitespace
+}
+impl TokenType {
+    pub fn to_string(&self) -> String {
+        match self {
+            TokenType::NumericLiteral => "NumericLiteral".to_string(),
+            TokenType::StringLiteral => "StringLiteral".to_string(),
+            TokenType::Whitespace => "Whitespace".to_string()
+        }
+    }
+}
+pub struct Token {
+    pub token_type: TokenType,
+    pub val: String
+}
+impl Token {
+    pub fn to_string(&self) -> String {
+        format!("
+            '{}': {{
+                val: {}
+            }}
+        ",self.token_type.to_string(),self.val)
+    }
+}
+#[derive(PartialEq, Eq, Hash, Clone)]
 pub struct Tokenizer {
     text: String,
-    pointer: usize
+    pointer: usize,
+    pub next_token: Option<String>
 }
 impl Tokenizer {
     pub fn new(text: &str) -> Tokenizer {
         Tokenizer {
             text: String::from(text),
-            pointer: 0
+            pointer: 0,
+            next_token: None
         }
     }
-    pub fn next_token(&mut self) -> Option<String> {
-        let mut result: Option<String> = None;
+    pub fn get_next_token(&mut self) -> Option<Token> {
+        let mut result: Option<Token> = None;
         let regex = [
-            ("NumericLiteral", Regex::new(r"^\d+(\.[\d]+)?").unwrap()),
-            ("Whitespace", Regex::new("^ +").unwrap()),
-            ("StringLiteral", Regex::new(r#"^"[^"\n]*""#).unwrap()),
-            ("StringLiteral", Regex::new(r#"^'[^'\n]*'"#).unwrap())
+            (TokenType::NumericLiteral, Regex::new(r"^\d+(\.[\d]+)?").unwrap()),
+            (TokenType::Whitespace, Regex::new("^ +").unwrap()),
+            (TokenType::StringLiteral, Regex::new(r#"^"[^"\n]*""#).unwrap()),
+            (TokenType::StringLiteral, Regex::new(r#"^'[^'\n]*'"#).unwrap())
         ];
         for (key, regex) in regex {
             if regex.is_match(&self.text) {
                 let mut val = regex.captures(&self.text).unwrap()[0].to_string();
                 self.text = String::from(&self.text[{val.len()}..]);
-                if key == "StringLiteral" {
+                if let TokenType::StringLiteral = key {
                     val = val[1..{val.len()-1}].to_string();
                 }
-                result = Some(json::stringify(object!{
-                    type: key.to_string(),
+                result = Some(Token{
+                    token_type: key,
                     val: val,
-                }));
+                });
                 break;
             }
         }
