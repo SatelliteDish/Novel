e4c156e9ba20fe0a7d85a65bfa5fcb8af35f5488
Michael V <michael.a.j.vitale@gmail.com>
2024-06-20 08:23:03 +0000

    added the ability to read command line arguments

--- novel/src/main.rs
+++ novel/src/main.rs
@@ -3,9 +3,27 @@ use std::fs::read_to_string;
 mod parser;
 
 fn main() {
-    let text = match read_to_string("./src/code/First.nvl")  {
-        Ok(val) => val,
-        Err(_e) => format!("Error: {}",_e),
+    let args: Vec<String> = std::env::args().collect();
+    
+    if args.len() < 2 {
+        println!("Requires path to file!");
+        return;
+    }
+
+    let text = match get_file(&args[1]) {
+        Ok(txt) => txt,
+        Err(e) => {
+            eprintln!("{}",e);
+            std::process::exit(1);
+        }
     };
+    println!("{}",std::env::current_dir().unwrap().display());
     println!("{}\n",parser::parse(&text));
+}
+
+fn get_file(path: &str) -> Result<String,String> { 
+    match read_to_string(path) {
+        Ok(txt) => Ok(txt),
+        Err(e) => Err(e.to_string())
+    }
 }
